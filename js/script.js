024e6c89fd3b382afa6a57626f7a5b7fdb0bded2
RhysJenkins89 <89rhysjenkins@gmail.com>
2020-11-11 10:02:12 +0000

    added js functionality

---
+++ js/script.js
@@ -0,0 +1,48 @@
+var hamButton = document.getElementById("button");
+
+var overlay = document.querySelector(".navigation__overlay");
+
+var links = document.querySelector(".navigation__links");
+
+var menuOpen = false;
+
+hamButton.addEventListener("click", function() {
+    if (!menuOpen) {
+        hamButton.classList.add("open");
+        overlay.classList.add("navigation__open")
+        links.classList.add("navigation__open");
+        menuOpen = true;
+    } else {
+        hamButton.classList.remove("open");
+        overlay.classList.remove("navigation__open");
+        links.classList.remove("navigation__open");
+        menuOpen = false;
+    }
+});
+
+overlay.addEventListener("click", function () {
+    overlay.classList.remove("navigation__open");
+    hamButton.classList.remove("open");
+    links.classList.remove("navigation__open");    
+});
+
+links.addEventListener("click", () => {
+    overlay.classList.remove("navigation__open");
+    links.classList.remove("navigation__open");
+    hamButton.classList.remove("open");
+});
+
+var winTest = window.location.toString();
+
+var newWin = winTest.split("#")[0];
+
+// The following removes the global transition: none once the page has loaded.  
+window.addEventListener("load", () => {
+    document.body.classList.remove("preload");
+});
+
+//Remove the refresh id issue.
+
+// Clean all this up!
+
+
