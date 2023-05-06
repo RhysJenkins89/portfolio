bbdb569c9c40640d17b6d5ed8ea620367782407f
Rhys Jenkins <89rhysjenkins@gmail.com>
2023-05-06 11:11:43 +0100

    Colours, styles

--- js/script.js
+++ js/script.js
@@ -11,12 +11,20 @@ toggleButton.addEventListener('click', () => {
     const headingsLevelOne = document.querySelectorAll('h1');
     const headingsLevelTwo = document.querySelectorAll('h2');
     const paragraphs = document.querySelectorAll('.text');
-    const textElements = [];
-    textElements.push(headingsLevelOne, headingsLevelTwo, paragraphs);
+    console.log(paragraphs);
+    // Note that here I loop over the elements that will need the light-mode class. There's probably a better way to do this. 
+    // const textElements = [];
+    // textElements.push(headingsLevelOne, headingsLevelTwo, paragraphs);
     if (!isDarkMode) {
         mainBody.classList.toggle('light-mode');
-        textElements.forEach(element => {
-            element[0].classList.toggle('light-mode');
+        headingsLevelOne.forEach(element => {
+            element.classList.toggle('light-mode');
+        });
+        headingsLevelTwo.forEach(element => {
+            element.classList.toggle('light-mode');
+        });
+        paragraphs.forEach(element => {
+            element.classList.toggle('light-mode');
         });
     }
 });
--- readme.md
+++ readme.md
@@ -4,7 +4,9 @@ I'm currently redesigning and rebuilding my portfolio site. Come back soon!
 
 ## To do
 
-* Add a light mode/dark mode toggle
+* Add a projects/what I doing section
+* Move the dark/light mode toggle to the nav
+* Find light/dark logos
 * Determine the user's light/dark mode preference
 * Add colour themes
 * Add a form, which when submitted sends the user's data to a database
