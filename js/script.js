87e1da1fa0825dd2fa160796577aedea79b68a34
Rhys Jenkins <89rhysjenkins@gmail.com>
2023-05-07 10:05:16 +0100

    Added dark/light mode icons

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ let isDarkMode = true;
 toggleTheme.addEventListener('click', () => {
     if (isDarkMode) {
         isDarkMode = !isDarkMode;
-        toggleTheme.innerText = 'Dark'
+        document.querySelector('.moon-icon').style.display = 'block'
+        document.querySelector('.sun-icon').style.display = 'none'
+        console.log('light mode is on')
     } else {
         isDarkMode = !isDarkMode;
-        toggleTheme.innerText = 'Light'
+        // toggleTheme.innerText = 'Light'
+        document.querySelector('.moon-icon').style.display = 'none'
+        document.querySelector('.sun-icon').style.display = 'block'
+        console.log('Dark mode is on')
     }
 
     // setTheme function
--- readme.md
+++ readme.md
@@ -7,7 +7,6 @@ I'm currently redesigning and rebuilding my portfolio site. Come back soon!
 * Sort the text size transition on initial page load
 * Sort the nav items one way or the other
 * Add a projects/what I doing section
-* Find light/dark logos
 * Determine the user's light/dark mode preference
 * Add colour themes
 * Build a better light mode theme
@@ -15,3 +14,4 @@ I'm currently redesigning and rebuilding my portfolio site. Come back soon!
 * Build a backend with email/text submission functionality
 * Build/find a system for the correct font-size values across devices
 * Build better button styles
+* Fade in/out dark/light mode icons on click
