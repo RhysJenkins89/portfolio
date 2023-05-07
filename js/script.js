95f2d63d10f182b49b81854ebfc06ed273c043d5
Rhys Jenkins <89rhysjenkins@gmail.com>
2023-05-07 10:17:27 +0100

    Tidied up JS file

--- js/script.js
+++ js/script.js
@@ -1,31 +1,21 @@
 // Dark/light mode toggle
 const toggleTheme = document.querySelector('#darkModeToggle');
 let isDarkMode = true;
+const sunIcon = document.querySelector('.sun-icon'); 
+const moonIcon = document.querySelector('.moon-icon');
 toggleTheme.addEventListener('click', () => {
     if (isDarkMode) {
         isDarkMode = !isDarkMode;
-        document.querySelector('.moon-icon').style.display = 'block'
-        document.querySelector('.sun-icon').style.display = 'none'
+        moonIcon.style.display = 'block'
+        sunIcon.style.display = 'none'
     } else {
         isDarkMode = !isDarkMode;
-        document.querySelector('.moon-icon').style.display = 'none'
-        document.querySelector('.sun-icon').style.display = 'block'
+        moonIcon.style.display = 'none'
+        sunIcon.style.display = 'block'
     }
 
-    // setTheme function
-    const mainBody = document.querySelector('#body');
-    const headingsLevelOne = document.querySelectorAll('h1');
-    const headingsLevelTwo = document.querySelectorAll('h2');
-    const paragraphs = document.querySelectorAll('.text');
-    // Note that here I loop over the elements that will need the light-mode class. There's probably a better way to do this. 
-    mainBody.classList.toggle('light-mode');
-    headingsLevelOne.forEach(element => {
-        element.classList.toggle('light-mode');
-    });
-    headingsLevelTwo.forEach(element => {
-        element.classList.toggle('light-mode');
-    });
-    paragraphs.forEach(element => {
-        element.classList.toggle('light-mode');
-    });
+    document.querySelector('#body').classList.toggle('light-mode');
+    document.querySelectorAll('h1').forEach(element => element.classList.toggle('light-mode'));
+    document.querySelectorAll('h2').forEach(element => element.classList.toggle('light-mode'));
+    document.querySelectorAll('.text').forEach(element => element.classList.toggle('light-mode'));
 });
