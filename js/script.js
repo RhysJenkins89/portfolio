cdfbe7082a90c833bcc5deba4ca64ed31fdd3228
Rhys Jenkins <89rhysjenkins@gmail.com>
2023-05-07 10:08:01 +0100

    Removed .logs

--- js/script.js
+++ js/script.js
@@ -6,13 +6,10 @@ toggleTheme.addEventListener('click', () => {
         isDarkMode = !isDarkMode;
         document.querySelector('.moon-icon').style.display = 'block'
         document.querySelector('.sun-icon').style.display = 'none'
-        console.log('light mode is on')
     } else {
         isDarkMode = !isDarkMode;
-        // toggleTheme.innerText = 'Light'
         document.querySelector('.moon-icon').style.display = 'none'
         document.querySelector('.sun-icon').style.display = 'block'
-        console.log('Dark mode is on')
     }
 
     // setTheme function
@@ -20,10 +17,7 @@ toggleTheme.addEventListener('click', () => {
     const headingsLevelOne = document.querySelectorAll('h1');
     const headingsLevelTwo = document.querySelectorAll('h2');
     const paragraphs = document.querySelectorAll('.text');
-    console.log(paragraphs);
     // Note that here I loop over the elements that will need the light-mode class. There's probably a better way to do this. 
-    // const textElements = [];
-    // textElements.push(headingsLevelOne, headingsLevelTwo, paragraphs);
     mainBody.classList.toggle('light-mode');
     headingsLevelOne.forEach(element => {
         element.classList.toggle('light-mode');
