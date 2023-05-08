3b2e0385108a1a626a3ae06d9faba5c878c73fe3
Rhys <rhys@rhysjenkins.uk>
2023-05-08 13:04:35 +0100

    Icon fade logic, removed for now

--- js/script.js
+++ js/script.js
@@ -1,21 +1,64 @@
 // Dark/light mode toggle
 const toggleTheme = document.querySelector('#darkModeToggle');
 let isDarkMode = true;
+const moonContainer = document.querySelector('.moon-container');
+const sunContainer = document.querySelector('.sun-container');
 const sunIcon = document.querySelector('.sun-icon'); 
 const moonIcon = document.querySelector('.moon-icon');
 toggleTheme.addEventListener('click', () => {
-    if (isDarkMode) {
-        isDarkMode = !isDarkMode;
-        moonIcon.style.display = 'block'
-        sunIcon.style.display = 'none'
-    } else {
-        isDarkMode = !isDarkMode;
-        moonIcon.style.display = 'none'
-        sunIcon.style.display = 'block'
-    }
+    // if (isDarkMode) {
+    //     isDarkMode = !isDarkMode;
+    //     // moonIcon.style.display = 'block'
+    //     // sunIcon.style.display = 'none'
+    //     // moonContainer.classList.toggle('fade-out');
+    //     // moonIcon.classList.toggle('fade-out');
+    //     // sunContainer.classList.remove('moved');
+    //     // sunIcon.classList.remove('moved');
+    // } else {
+    //     isDarkMode = !isDarkMode;
+    //     // moonIcon.style.display = 'none'
+    //     // sunIcon.style.display = 'block'
+    //     moonContainer.classList.toggle('fade');
+    //     moonIcon.classList.toggle('fade');
+    //     sunIcon.classList.toggle('fade');
+    // }
 
-    document.querySelector('#body').classList.toggle('light-mode');
-    document.querySelectorAll('h1').forEach(element => element.classList.toggle('light-mode'));
-    document.querySelectorAll('h2').forEach(element => element.classList.toggle('light-mode'));
-    document.querySelectorAll('.text').forEach(element => element.classList.toggle('light-mode'));
+    // document.querySelector('#body').classList.toggle('light-mode');
+    // document.querySelectorAll('h1').forEach(element => element.classList.toggle('light-mode'));
+    // document.querySelectorAll('h2').forEach(element => element.classList.toggle('light-mode'));
+    // document.querySelectorAll('.text').forEach(element => element.classList.toggle('light-mode'));
 });
+
+sunContainer.addEventListener('click', () => {
+    sunContainer.style.transition = 'all 1s ease-out'
+    sunIcon.style.transition = 'all 1s ease-in'
+    sunContainer.classList.add('fade-out');
+    sunIcon.classList.add('fade-out');
+    moonContainer.classList.remove('moved');
+    moonIcon.classList.remove('moved');
+    moonContainer.classList.remove('fade-out');
+    moonIcon.classList.remove('fade-out');
+    setTimeout(() => {
+        sunContainer.classList.add('moved');
+        sunIcon.classList.add('moved');
+        sunContainer.style.transition = 'all 1s ease-in';
+        sunIcon.style.transition = 'all 1s ease-out';
+    }, 1000);
+})
+
+moonContainer.addEventListener('click', () => {
+    moonContainer.style.transition = 'all 1s ease-out'
+    moonIcon.style.transition = 'all 1s ease-in'
+    moonContainer.classList.add('fade-out');
+    moonIcon.classList.add('fade-out');
+    sunContainer.classList.remove('moved');
+    sunIcon.classList.remove('moved');
+    sunContainer.classList.remove('fade-out');
+    sunIcon.classList.remove('fade-out');
+    setTimeout(() => {
+        moonContainer.classList.add('moved');
+        moonIcon.classList.add('moved');
+        moonContainer.style.transition = 'all 1s ease-in'
+        moonIcon.style.transition = 'all 1s ease-out'
+    }, 1000);
+})
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@ I'm currently redesigning and rebuilding my portfolio site. Come back soon!
 
 ## To do
 
+* Fade in/out dark/light mode icons on click
+* Fade in/out icons along a curve
 * Sort the text size transition on initial page load
 * Sort the nav items one way or the other
 * Add a projects/what I doing section
@@ -14,6 +16,6 @@ I'm currently redesigning and rebuilding my portfolio site. Come back soon!
 * Build a backend with email/text submission functionality
 * Build/find a system for the correct font-size values across devices
 * Build better button styles
-* Fade in/out dark/light mode icons on click
 * Change the browser tab icon colour depending on the browser theme
 * To stop the CSS transition, perhaps only load the JS after page load
+* Write page landing copy
