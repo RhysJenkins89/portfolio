4fa643a594c3ec058980c0d69fdfb8f922e47103
Rhys <rhys@rhysjenkins.uk>
2023-07-13 06:45:37 +0100

    Updated listener function

--- dist/index.js
+++ dist/index.js
@@ -55,88 +55,119 @@ function darkLightToggle() {
         });
     }
     var clickDisabled = false;
-    // function addListeners(element: HTMLElement, icon: HTMLElement, event: string): void {
-    // element.addEventListener(event, () => {
-    // if (clickDisabled) return
-    // if (element.classList.contains("moved")) return
-    // })
-    // clickDisabled = true
-    // }
+    function addListeners(elementOne, iconOne, elementTwo, iconTwo) {
+        elementOne.addEventListener('click', function () {
+            if (clickDisabled)
+                return;
+            if (elementOne.classList.contains("moved"))
+                return;
+            clickDisabled = true;
+            // one: sun
+            // two: moon
+            elementOne.style.transition = "all 1s ease-out";
+            iconOne.style.transition = "all 1s ease-in";
+            // Move out and fade out the sun
+            elementOne.classList.add("fade-out");
+            iconOne.classList.add("fade-out");
+            // Move in and fade in the moon
+            elementTwo.classList.remove("moved");
+            iconTwo.classList.remove("moved");
+            // If the moon contains the fade-out class, remove it
+            if (elementTwo.classList.contains("fade-out")) {
+                elementTwo.classList.remove("fade-out");
+                iconTwo.classList.remove("fade-out");
+            }
+            // Add a pointer cursor to the moon and remove it from the sun
+            iconTwo.style.cursor = "pointer";
+            iconOne.style.cursor = "default";
+            toggleTheme();
+            setTimeout(function () {
+                // After one second, move the sun to correct position
+                elementOne.classList.add("moved");
+                iconOne.classList.add("moved");
+                // Add the correct transition styles
+                elementOne.style.transition = "all 1s ease-in";
+                iconOne.style.transition = "all 1s ease-out";
+            }, 1000);
+            // Return click events
+            setTimeout(function () {
+                clickDisabled = false;
+            }, 2000);
+        });
+    }
+    addListeners(sunContainer, sunIcon, moonContainer, moonIcon);
+    addListeners(moonContainer, moonIcon, sunContainer, sunIcon);
     // There's also a lof of repeated code in the following two functions. I suggest that we consoldate them into one function.
-    sunContainer.addEventListener("click", function () {
-        if (clickDisabled)
-            return;
-        if (sunContainer.classList.contains("moved"))
-            return;
-        clickDisabled = true;
-        // Return to correct transition styles if necessary
-        sunContainer.style.transition = "all 1s ease-out";
-        sunIcon.style.transition = "all 1s ease-in";
-        // Move out and fade out the sun
-        sunContainer.classList.add("fade-out");
-        sunIcon.classList.add("fade-out");
-        // Move in and fade in the moon
-        moonContainer.classList.remove("moved");
-        moonIcon.classList.remove("moved");
-        // If the moon contains the fade-out class, remove it
-        if (moonContainer.classList.contains("fade-out")) {
-            moonContainer.classList.remove("fade-out");
-            moonIcon.classList.remove("fade-out");
-        }
-        // Add a pointer cursor to the moon and remove it from the sun
-        moonIcon.style.cursor = "pointer";
-        sunIcon.style.cursor = "default";
-        toggleTheme();
-        setTimeout(function () {
-            // After one second, move the sun to correct position
-            sunContainer.classList.add("moved");
-            sunIcon.classList.add("moved");
-            // Add the correct transition styles
-            sunContainer.style.transition = "all 1s ease-in";
-            sunIcon.style.transition = "all 1s ease-out";
-        }, 1000);
-        // Return click events
-        setTimeout(function () {
-            clickDisabled = false;
-        }, 2000);
-    });
-    moonContainer.addEventListener("click", function () {
-        if (clickDisabled)
-            return;
-        if (moonContainer.classList.contains("moved"))
-            return;
-        clickDisabled = true;
-        // Return to correct transition styles if necessary
-        moonContainer.style.transition = "all 1s ease-out";
-        moonIcon.style.transition = "all 1s ease-in";
-        // Move out and fade out the moon
-        moonContainer.classList.add("fade-out");
-        moonIcon.classList.add("fade-out");
-        // Move in and fade in the sun
-        sunContainer.classList.remove("moved");
-        sunIcon.classList.remove("moved");
-        // If the sun contains the fade-out class, remove it
-        if (sunContainer.classList.contains("fade-out")) {
-            sunContainer.classList.remove("fade-out");
-            sunIcon.classList.remove("fade-out");
-        }
-        // Add a pointer cursor to the sun and remove it from the moon
-        sunIcon.style.cursor = "pointer";
-        moonIcon.style.cursor = "default";
-        toggleTheme();
-        setTimeout(function () {
-            // After one second, move the moon to the correct postition
-            moonContainer.classList.add("moved");
-            moonIcon.classList.add("moved");
-            // Add the correct transition styles
-            moonContainer.style.transition = "all 1s ease-in";
-            moonIcon.style.transition = "all 1s ease-out";
-        }, 1000);
-        // Return click events
-        setTimeout(function () {
-            clickDisabled = false;
-        }, 2000);
-    });
+    // sunContainer.addEventListener("click", () => {
+    //     if (clickDisabled) return
+    //     if (sunContainer.classList.contains("moved")) return
+    //     clickDisabled = true
+    //     // Return to correct transition styles if necessary
+    //     sunContainer.style.transition = "all 1s ease-out"
+    //     sunIcon.style.transition = "all 1s ease-in"
+    //     // Move out and fade out the sun
+    //     sunContainer.classList.add("fade-out")
+    //     sunIcon.classList.add("fade-out")
+    //     // Move in and fade in the moon
+    //     moonContainer.classList.remove("moved")
+    //     moonIcon.classList.remove("moved")
+    //     // If the moon contains the fade-out class, remove it
+    //     if (moonContainer.classList.contains("fade-out")) {
+    //         moonContainer.classList.remove("fade-out")
+    //         moonIcon.classList.remove("fade-out")
+    //     }
+    //     // Add a pointer cursor to the moon and remove it from the sun
+    //     moonIcon.style.cursor = "pointer"
+    //     sunIcon.style.cursor = "default"
+    //     toggleTheme()
+    //     setTimeout(() => {
+    //         // After one second, move the sun to correct position
+    //         sunContainer.classList.add("moved")
+    //         sunIcon.classList.add("moved")
+    //         // Add the correct transition styles
+    //         sunContainer.style.transition = "all 1s ease-in"
+    //         sunIcon.style.transition = "all 1s ease-out"
+    //     }, 1000)
+    //     // Return click events
+    //     setTimeout(() => {
+    //         clickDisabled = false
+    //     }, 2000)
+    // })
+    // moonContainer.addEventListener("click", () => {
+    //     if (clickDisabled) return
+    //     if (moonContainer.classList.contains("moved")) return
+    //     clickDisabled = true
+    //     // Return to correct transition styles if necessary
+    //     moonContainer.style.transition = "all 1s ease-out"
+    //     moonIcon.style.transition = "all 1s ease-in"
+    //     // Move out and fade out the moon
+    //     moonContainer.classList.add("fade-out")
+    //     moonIcon.classList.add("fade-out")
+    //     // Move in and fade in the sun
+    //     sunContainer.classList.remove("moved")
+    //     sunIcon.classList.remove("moved")
+    //     // If the sun contains the fade-out class, remove it
+    //     if (sunContainer.classList.contains("fade-out")) {
+    //         sunContainer.classList.remove("fade-out")
+    //         sunIcon.classList.remove("fade-out")
+    //     }
+    //     // Add a pointer cursor to the sun and remove it from the moon
+    //     sunIcon.style.cursor = "pointer"
+    //     moonIcon.style.cursor = "default"
+    //     toggleTheme()
+    //     setTimeout(() => {
+    //         // After one second, move the moon to the correct postition
+    //         moonContainer.classList.add("moved")
+    //         moonIcon.classList.add("moved")
+    //         // Add the correct transition styles
+    //         moonContainer.style.transition = "all 1s ease-in"
+    //         moonIcon.style.transition = "all 1s ease-out"
+    //     }, 1000)
+    //     // Return click events
+    //     setTimeout(() => {
+    //         clickDisabled = false
+    //     }, 2000)
+    // })
 }
 function renderProjects() {
     return __awaiter(this, void 0, void 0, function () {
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ const sunContainer = document.querySelector(".sun-container") as HTMLElement
 const sunIcon = document.querySelector(".sun-icon") as HTMLElement
 const moonIcon = document.querySelector(".moon-icon") as HTMLElement
 const bodyElement = document.querySelector("#body") as HTMLElement
-const textElements = document.querySelectorAll(".text") as NodeListOf<HTMLElement>
+const textElements = document.querySelectorAll(
+    ".text"
+) as NodeListOf<HTMLElement>
 
 window.onload = (event) => {
     bodyElement.style.transition = "all 1s ease"
-    textElements.forEach(element => {
+    textElements.forEach((element) => {
         element.style.transition = "all 1s ease"
     })
 }
@@ -22,106 +24,156 @@ function darkLightToggle(): void {
 
     let clickDisabled: boolean = false
 
-    // function addListeners(element: HTMLElement, icon: HTMLElement, event: string): void {
-        // element.addEventListener(event, () => {
-            // if (clickDisabled) return
-            // if (element.classList.contains("moved")) return
-        // })
-        // clickDisabled = true
-    // }
+    function addListeners(
+        elementOne: HTMLElement,
+        iconOne: HTMLElement,
+        elementTwo: HTMLElement,
+        iconTwo: HTMLElement
+    ): void {
+        elementOne.addEventListener('click', () => {
+            if (clickDisabled) return
+            if (elementOne.classList.contains("moved")) return
+        
+            clickDisabled = true
+
+            // one: sun
+            // two: moon
+
+            elementOne.style.transition = "all 1s ease-out"
+            iconOne.style.transition = "all 1s ease-in"
+
+            // Move out and fade out the sun
+            elementOne.classList.add("fade-out")
+            iconOne.classList.add("fade-out")
+
+            // Move in and fade in the moon
+            elementTwo.classList.remove("moved")
+            iconTwo.classList.remove("moved")
+
+            // If the moon contains the fade-out class, remove it
+            if (elementTwo.classList.contains("fade-out")) {
+                elementTwo.classList.remove("fade-out")
+                iconTwo.classList.remove("fade-out")
+            }
+
+            // Add a pointer cursor to the moon and remove it from the sun
+            iconTwo.style.cursor = "pointer"
+            iconOne.style.cursor = "default"
+
+            toggleTheme()
+
+            setTimeout(() => {
+                // After one second, move the sun to correct position
+                elementOne.classList.add("moved")
+                iconOne.classList.add("moved")
+
+                // Add the correct transition styles
+                elementOne.style.transition = "all 1s ease-in"
+                iconOne.style.transition = "all 1s ease-out"
+            }, 1000)
+
+            // Return click events
+            setTimeout(() => {
+                clickDisabled = false
+            }, 2000)
+        })
+    }
+
+    addListeners(sunContainer, sunIcon, moonContainer, moonIcon)
+    addListeners(moonContainer, moonIcon, sunContainer, sunIcon)
 
     // There's also a lof of repeated code in the following two functions. I suggest that we consoldate them into one function.
-    sunContainer.addEventListener("click", () => {
-        if (clickDisabled) return
-        if (sunContainer.classList.contains("moved")) return
+    // sunContainer.addEventListener("click", () => {
+    //     if (clickDisabled) return
+    //     if (sunContainer.classList.contains("moved")) return
 
-        clickDisabled = true
+    //     clickDisabled = true
 
-        // Return to correct transition styles if necessary
-        sunContainer.style.transition = "all 1s ease-out"
-        sunIcon.style.transition = "all 1s ease-in"
+    //     // Return to correct transition styles if necessary
+    //     sunContainer.style.transition = "all 1s ease-out"
+    //     sunIcon.style.transition = "all 1s ease-in"
 
-        // Move out and fade out the sun
-        sunContainer.classList.add("fade-out")
-        sunIcon.classList.add("fade-out")
+    //     // Move out and fade out the sun
+    //     sunContainer.classList.add("fade-out")
+    //     sunIcon.classList.add("fade-out")
 
-        // Move in and fade in the moon
-        moonContainer.classList.remove("moved")
-        moonIcon.classList.remove("moved")
+    //     // Move in and fade in the moon
+    //     moonContainer.classList.remove("moved")
+    //     moonIcon.classList.remove("moved")
 
-        // If the moon contains the fade-out class, remove it
-        if (moonContainer.classList.contains("fade-out")) {
-            moonContainer.classList.remove("fade-out")
-            moonIcon.classList.remove("fade-out")
-        }
+    //     // If the moon contains the fade-out class, remove it
+    //     if (moonContainer.classList.contains("fade-out")) {
+    //         moonContainer.classList.remove("fade-out")
+    //         moonIcon.classList.remove("fade-out")
+    //     }
 
-        // Add a pointer cursor to the moon and remove it from the sun
-        moonIcon.style.cursor = "pointer"
-        sunIcon.style.cursor = "default"
+    //     // Add a pointer cursor to the moon and remove it from the sun
+    //     moonIcon.style.cursor = "pointer"
+    //     sunIcon.style.cursor = "default"
 
-        toggleTheme()
+    //     toggleTheme()
 
-        setTimeout(() => {
-            // After one second, move the sun to correct position
-            sunContainer.classList.add("moved")
-            sunIcon.classList.add("moved")
+    //     setTimeout(() => {
+    //         // After one second, move the sun to correct position
+    //         sunContainer.classList.add("moved")
+    //         sunIcon.classList.add("moved")
 
-            // Add the correct transition styles
-            sunContainer.style.transition = "all 1s ease-in"
-            sunIcon.style.transition = "all 1s ease-out"
-        }, 1000)
+    //         // Add the correct transition styles
+    //         sunContainer.style.transition = "all 1s ease-in"
+    //         sunIcon.style.transition = "all 1s ease-out"
+    //     }, 1000)
 
-        // Return click events
-        setTimeout(() => {
-            clickDisabled = false
-        }, 2000)
-    })
+    //     // Return click events
+    //     setTimeout(() => {
+    //         clickDisabled = false
+    //     }, 2000)
+    // })
 
-    moonContainer.addEventListener("click", () => {
-        if (clickDisabled) return
-        if (moonContainer.classList.contains("moved")) return
+    // moonContainer.addEventListener("click", () => {
+    //     if (clickDisabled) return
+    //     if (moonContainer.classList.contains("moved")) return
 
-        clickDisabled = true
+    //     clickDisabled = true
 
-        // Return to correct transition styles if necessary
-        moonContainer.style.transition = "all 1s ease-out"
-        moonIcon.style.transition = "all 1s ease-in"
+    //     // Return to correct transition styles if necessary
+    //     moonContainer.style.transition = "all 1s ease-out"
+    //     moonIcon.style.transition = "all 1s ease-in"
 
-        // Move out and fade out the moon
-        moonContainer.classList.add("fade-out")
-        moonIcon.classList.add("fade-out")
+    //     // Move out and fade out the moon
+    //     moonContainer.classList.add("fade-out")
+    //     moonIcon.classList.add("fade-out")
 
-        // Move in and fade in the sun
-        sunContainer.classList.remove("moved")
-        sunIcon.classList.remove("moved")
+    //     // Move in and fade in the sun
+    //     sunContainer.classList.remove("moved")
+    //     sunIcon.classList.remove("moved")
 
-        // If the sun contains the fade-out class, remove it
-        if (sunContainer.classList.contains("fade-out")) {
-            sunContainer.classList.remove("fade-out")
-            sunIcon.classList.remove("fade-out")
-        }
+    //     // If the sun contains the fade-out class, remove it
+    //     if (sunContainer.classList.contains("fade-out")) {
+    //         sunContainer.classList.remove("fade-out")
+    //         sunIcon.classList.remove("fade-out")
+    //     }
 
-        // Add a pointer cursor to the sun and remove it from the moon
-        sunIcon.style.cursor = "pointer"
-        moonIcon.style.cursor = "default"
+    //     // Add a pointer cursor to the sun and remove it from the moon
+    //     sunIcon.style.cursor = "pointer"
+    //     moonIcon.style.cursor = "default"
 
-        toggleTheme()
+    //     toggleTheme()
 
-        setTimeout(() => {
-            // After one second, move the moon to the correct postition
-            moonContainer.classList.add("moved")
-            moonIcon.classList.add("moved")
+    //     setTimeout(() => {
+    //         // After one second, move the moon to the correct postition
+    //         moonContainer.classList.add("moved")
+    //         moonIcon.classList.add("moved")
 
-            // Add the correct transition styles
-            moonContainer.style.transition = "all 1s ease-in"
-            moonIcon.style.transition = "all 1s ease-out"
-        }, 1000)
+    //         // Add the correct transition styles
+    //         moonContainer.style.transition = "all 1s ease-in"
+    //         moonIcon.style.transition = "all 1s ease-out"
+    //     }, 1000)
 
-        // Return click events
-        setTimeout(() => {
-            clickDisabled = false
-        }, 2000)
-    })
+    //     // Return click events
+    //     setTimeout(() => {
+    //         clickDisabled = false
+    //     }, 2000)
+    // })
 }
 
 interface ProjectsData {
