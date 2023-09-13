f57cadc643d5af099f0a4bf1cdddd258b8d04158
Rhys <rhys@rhysjenkins.uk>
2023-09-13 17:34:00 +0100

    Uploading older code

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,6 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const theme_1 = __importDefault(require("./theme"));
+const theme_1 = require("./theme");
 (0, theme_1.default)();
 const rootElement = document.querySelector("html");
 const overlay = document.querySelector(".overlay");
