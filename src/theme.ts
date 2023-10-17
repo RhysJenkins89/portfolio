a32f76181f9aca21ae41c331d06a7ff9dc196985
Rhys <rhys@rhysjenkins.uk>
2023-10-17 20:19:28 +0100

    Finished light/dark logic

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,7 @@
 const rootElement = document.querySelector("html") as HTMLElement;
-const themeSelector = document.querySelector(".theme-text") as HTMLElement;
-const themeImages = document.querySelectorAll("[data-theme-toggle]") as NodeListOf<HTMLElement>; // themeIcons
+const themeIcons = document.querySelectorAll("[data-theme-toggle]") as NodeListOf<HTMLElement>;
+const sunIcon = document.querySelector(".sun") as HTMLElement;
+const moonIcon = document.querySelector(".moon") as HTMLElement;
 const userHasSelectedTheme: boolean = Boolean(window.localStorage.getItem("themeSelection"));
 let isDarkMode: boolean = true;
 
@@ -15,42 +16,39 @@ window.onload = () => {
 function themeToggle(): void {
     checkUserThemePreference();
 
-    themeImages.forEach((image) => {
-        image.addEventListener("click", () => {
-            // Put this code into its own function
-            if (isDarkMode) {
-                document.querySelector(".sun")!.classList.add("hidden"); // Potentially guard here, but we're sorted
-                rootElement.dataset.theme = "light";
-                window.localStorage.setItem("themeSelection", "light");
-                isDarkMode = false;
-            } else {
-                rootElement.dataset.theme = "dark";
-                window.localStorage.setItem("themeSelection", "dark");
-                isDarkMode = true;
-            }
+    themeIcons.forEach((icons) => {
+        icons.addEventListener("click", () => {
+            updateTheme();
         });
     });
+}
 
-    themeSelector.addEventListener("click", () => {
-        // Put this code into its own function
-        if (isDarkMode) {
-            rootElement.dataset.theme = "light";
-            window.localStorage.setItem("themeSelection", "light");
-            isDarkMode = false;
-        } else {
-            rootElement.dataset.theme = "dark";
-            window.localStorage.setItem("themeSelection", "dark");
-            isDarkMode = true;
-        }
-    });
+function updateTheme(): void {
+    if (isDarkMode) {
+        sunIcon.classList.add("hidden");
+        moonIcon.classList.remove("hidden");
+        rootElement.dataset.theme = "light";
+        window.localStorage.setItem("themeSelection", "light");
+        isDarkMode = false;
+    } else {
+        sunIcon.classList.remove("hidden");
+        moonIcon.classList.add("hidden");
+        rootElement.dataset.theme = "dark";
+        window.localStorage.setItem("themeSelection", "dark");
+        isDarkMode = true;
+    }
 }
 
 function checkUserThemePreference(): void {
     if (!userHasSelectedTheme) return;
     rootElement.dataset.theme = window.localStorage.getItem("themeSelection")!;
     if (window.localStorage.getItem("themeSelection") === "dark") {
+        sunIcon.classList.remove("hidden");
+        moonIcon.classList.add("hidden");
         return;
     } else {
+        sunIcon.classList.add("hidden");
+        moonIcon.classList.remove("hidden");
         isDarkMode = false;
     }
 }
