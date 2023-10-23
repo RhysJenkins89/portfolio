49372ffe036ced8bc182663189af1cb891de0320
Rhys <rhys@rhysjenkins.uk>
2023-10-23 22:01:38 +0100

    Messing around

--- src/maps.ts
+++ src/maps.ts
@@ -1,26 +1,14 @@
-// import { Loader } from "@googlemaps/js-api-loader";
-
-// const loader = new Loader({
-//     apiKey: "AIzaSyBMrZzNh5ZmSi52ep2byYIzIyuTg8vQWUs",
-//     version: "weekly",
-// });
-
-// loader.load().then(async () => {
-//     const { Map } = (await google.maps.importLibrary("maps")) as google.maps.MapsLibrary;
-//     map = new Map(document.getElementById("map") as HTMLElement, {
-//         center: { lat: -34.397, lng: 150.644 },
-//         zoom: 8,
-//     });
-// });
-
 let map: google.maps.Map;
+const mapElement = document.querySelector(".main-maps-container") as HTMLElement;
+
 async function initMap(): Promise<void> {
-    console.log("It's working maybe!");
     const { Map } = (await google.maps.importLibrary("maps")) as google.maps.MapsLibrary;
-    map = new Map(document.querySelector(".main-maps-container") as HTMLElement, {
+    map = new Map(mapElement, {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8,
     });
 }
 
 export default initMap;
+
+// Remove the text from the map, leaving only the country borders. The user is given a country, and he must guess where the country is on the map. After the user guesses, the country is then revealed.
