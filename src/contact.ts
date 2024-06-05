10549fe0e7f5050dc5809941f31ba4c339720bfc
Rhys <rhys@rhysjenkins.uk>
2024-06-05 22:01:13 +0100

    Do I even need a form?

--- src/contact.ts
+++ src/contact.ts
@@ -1,12 +1,17 @@
 const form = document.querySelector("[data-form-submit]") as HTMLFormElement;
 const emailInput = document.querySelector("#email") as HTMLInputElement;
+const emailErrorText = document.querySelector("[data-email-error-text]") as HTMLElement;
 
-if (emailInput) {
+if (emailInput && emailErrorText) {
     emailInput.addEventListener("input", (event: Event) => {
+        // Form validation
+        console.log("Email input");
         if (emailInput.validity.typeMismatch) {
             emailInput.classList.add("invalid");
+            emailErrorText.classList.add("visible");
         } else {
             emailInput.classList.remove("invalid");
+            emailErrorText.classList.remove("visible");
         }
     });
 }
