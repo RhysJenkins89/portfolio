ef7bd039241585ae51cb0844eb7e465302d4dd4a
Rhys <rhys@rhysjenkins.uk>
2024-06-08 17:37:02 +0100

    Removing form

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import themeToggle from "./theme";
 // import renderProjects from "./projects";
-import handleFormSubmit from "./contact";
 
 themeToggle();
 //renderProjects();
-handleFormSubmit();
