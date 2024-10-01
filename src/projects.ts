0222b3fce1b5fa71ff5cfcfa650ed43d031eca93
Rhys <rhys@rhysjenkins.uk>
2024-10-01 20:41:32 +0100

    Minor updates

--- src/projects.ts
+++ src/projects.ts
@@ -3,24 +3,26 @@ import { ProjectsData } from "./types/projects-types";
 const cardsContainer = document.querySelector(".main-projects-container") as HTMLElement;
 
 async function renderProjects(): Promise<void> {
-    try {
-        const response: Response = await fetch("./dat/projects.json");
-        const projectData: ProjectsData = await response.json();
-        for (const project in projectData) {
-            cardsContainer.insertAdjacentHTML(
-                "beforeend",
-                `
-                    <div class="project">
-                        <h3 class="text project-heading">${projectData[project].title}</h3>
-                        <p class="text project-subheading">${projectData[project].subheading}</p>
-                        <p class="text project-text">${projectData[project].bodyText}</p>
-                    </div>
-                `
-            );
+    if (cardsContainer) {
+        try {
+            const response: Response = await fetch("./dat/projects.json");
+            const projectData: ProjectsData = await response.json();
+            for (const project in projectData) {
+                cardsContainer.insertAdjacentHTML(
+                    "beforeend",
+                    `
+                        <div class="project">
+                            <h3 class="text project-heading">${projectData[project].title}</h3>
+                            <p class="text project-subheading">${projectData[project].subheading}</p>
+                            <p class="text project-text">${projectData[project].bodyText}</p>
+                        </div>
+                    `
+                );
+            }
+        } catch (error) {
+            console.error(error);
+            cardsContainer.insertAdjacentHTML("beforeend", "<p>No data found.</p>");
         }
-    } catch (error) {
-        console.log(error);
-        cardsContainer.insertAdjacentHTML("beforeend", "<p>No data found.</p>");
     }
 }
 
